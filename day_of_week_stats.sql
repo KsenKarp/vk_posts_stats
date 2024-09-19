01948501dd75ad56ba69db53f4687359ce7342fb
Карпова Ксения <ksurompscov@mail.ru>
2024-09-19 12:00:12 +0300

    Update day_of_week_stats.sql

--- day_of_week_stats.sql
+++ day_of_week_stats.sql
@@ -3,18 +3,15 @@
 от дня недели. Для красоты графика вмсето цифр, которые возвращает 
 EXTRACT(ISODOW FROM publish_datetime::date) прописывала названия дней недели.
 */
-CREATE OR REPLACE VIEW day_of_week AS (
-	SELECT AVG(likes) as avg_likes_count, 
-	CASE 
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 1 THEN 'monday'
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 2  THEN 'tuesday'
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 3  THEN 'wednesday'
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 4  THEN 'thursday'
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 5  THEN 'friday'
-		WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 6  THEN 'saturday'
-		ELSE 'sunday'
-	END AS weekday
-	FROM posts_stats
-	GROUP BY weekday
-);
-SELECT * FROM day_of_week
+SELECT AVG(likes) as avg_likes_count, 
+CASE 
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 1 THEN 'monday'
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 2  THEN 'tuesday'
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 3  THEN 'wednesday'
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 4  THEN 'thursday'
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 5  THEN 'friday'
+	WHEN EXTRACT(ISODOW FROM publish_datetime::date) = 6  THEN 'saturday'
+	ELSE 'sunday'
+END AS weekday
+FROM posts_stats
+GROUP BY weekday
