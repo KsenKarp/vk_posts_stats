f07d3d7a9017ceab7a35e3397d1db588c88af8a0
Карпова Ксения <ksurompscov@mail.ru>
2024-09-19 10:29:56 +0300

    Update day_interval_stats.sql

--- day_interval_stats.sql
+++ day_interval_stats.sql
@@ -1,13 +1,14 @@
-CREATE OR REPLACE VIEW time_of_day AS (
-	SELECT AVG(likes) as avg_likes_count,
-	CASE 
-		WHEN publish_datetime::time BETWEEN '05:00'::TIME AND '11:00'::TIME THEN 'morning'
-		WHEN publish_datetime::time BETWEEN '11:00:01'::TIME AND '17:00'::TIME THEN 'daytime'
-		WHEN publish_datetime::time BETWEEN '17:00:01'::TIME AND '23:00'::TIME THEN 'evening'
-		ELSE 'night'
-	END AS day_interval
-	FROM posts_stats
-	GROUP BY day_interval
-);
-
-SELECT * FROM time_of_day
+/*
+Запрос для построения графика зависимости среднего количетсва лайков
+от времени суток. Утро, день, вечер и ночь определены несколько вольно:
+утро с 5 до 11 часов, день с 11 до 17, вечер с 17 до 23, ну и ночь с 23 до 5.
+*/
+SELECT AVG(likes) as avg_likes_count,
+CASE 
+	WHEN publish_datetime::time BETWEEN '05:00'::TIME AND '11:00'::TIME THEN 'morning'
+	WHEN publish_datetime::time BETWEEN '11:00:01'::TIME AND '17:00'::TIME THEN 'daytime'
+	WHEN publish_datetime::time BETWEEN '17:00:01'::TIME AND '23:00'::TIME THEN 'evening'
+	ELSE 'night'
+END AS day_interval
+FROM posts_stats
+GROUP BY day_interval
